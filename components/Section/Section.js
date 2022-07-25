b577ed27435510c9c80af027c3e3f05a879c5f1c
Vlad Brok <vlad.brok99@gmail.com>
2022-07-25 18:31:16 +0300

    Make section icon optional

--- components/Section/Section.js
+++ components/Section/Section.js
@@ -1,10 +1,10 @@
 import styles from "./Section.module.scss";
 
-export default function Section({ title, Icon, children }) {
+export default function Section({ title, Icon = null, children }) {
   return (
     <section className={styles.container}>
       <h2 className={styles.title}>
-        <Icon className={styles.icon} /> {title}
+        {Icon && <Icon className={styles.icon} />} {title}
       </h2>
       {children}
     </section>
