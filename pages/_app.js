116338bea80e2641ba422396ac0a9a187c26e5a1
Vlad Brok <vlad.brok99@gmail.com>
2022-07-26 14:45:24 +0300

    Make menu shared across all pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,14 @@
 import "../styles/globals.scss";
+import Menu from "../components/Menu";
+import menuItems from "../data/menuItems";
+
+menuItems[0].isActive = true;
 
 export default function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <>
+      <Component {...pageProps} />
+      <Menu itemsData={menuItems} />
+    </>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,10 @@
 import styles from "./index.module.scss";
-import Menu from "../components/Menu";
 import Main from "../components/Main";
 import Footer from "../components/Footer";
-import menuItems from "../data/menuItems";
-import { useEffect, useRef } from "react";
-
-menuItems[0].isActive = true;
 
 export default function Home() {
   return (
     <>
-      <Menu itemsData={menuItems} />
       <Main />
       <Footer />
     </>
