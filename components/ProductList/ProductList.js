9dbc32fe81d65df50e3a6a0f40e6df6869ca6cb6
Vlad Brok <vlad.brok99@gmail.com>
2022-07-29 12:36:31 +0300

    Make category page

--- components/ProductList/ProductList.js
+++ components/ProductList/ProductList.js
@@ -6,16 +6,18 @@ import Category from "../Category/";
 import { byName } from "../../lib/categoryFinders";
 import Link from "next/link";
 
-export default function ProductList({ filter }) {
+export default function ProductList({ filter, includeCategory = true }) {
   const products = productsData.filter(filter).map(d => (
     <div key={d.id}>
       {/* fixme: dup with ProductPage */}
-      <Category
-        flexDirection="row"
-        imageSizeIncrease="0px"
-        name={d.category}
-        imageUrl={categoriesData.find(byName(d.category)).imageUrl}
-      />
+      {includeCategory && (
+        <Category
+          flexDirection="row"
+          imageSizeIncrease="0px"
+          name={d.category}
+          imageUrl={categoriesData.find(byName(d.category)).imageUrl}
+        />
+      )}
       <Link href={`/products/${d.id}`}>
         <a>
           <Product image={d.image} price={d.price}>
--- pages/categories/[name].js
+++ pages/categories/[name].js
@@ -1,8 +1,28 @@
+import Subpage from "../../components/Subpage";
+import Category from "../../components/Category";
+import ProductList from "../../components/ProductList";
+import categories from "../../data/categories.json";
+import { byName } from "../../lib/categoryFinders";
+import { byCategory } from "../../lib/productFilters";
 import { useRouter } from "next/router";
 
-export default function Category() {
+export default function CategoryPage() {
   const router = useRouter();
   const { name } = router.query;
 
-  return <h1>{name}</h1>;
+  return (
+    <Subpage
+      /* fixme: dup */
+      title={
+        <Category
+          flexDirection="row" // todo: make default
+          imageSizeIncrease="0px" // todo: make default
+          name={name}
+          imageUrl={categories.find(byName(name)).imageUrl} // todo: incapsulate
+        />
+      }
+    >
+      <ProductList filter={byCategory(name)} includeCategory={false} />
+    </Subpage>
+  );
 }
