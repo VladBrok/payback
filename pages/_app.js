0ed66a467b1eee31cb71006b3fd5ccbe8eac746c
Vlad Brok <vlad.brok99@gmail.com>
2022-07-30 15:10:44 +0300

    Add page loading progress indicator

--- pages/_app.js
+++ pages/_app.js
@@ -2,8 +2,18 @@ import "../styles/globals.scss";
 import Menu from "../components/Menu";
 import Container from "../components/Container";
 import menuItems from "../data/menuItems";
+import ProgressBar from "@badrap/bar-of-progress";
+import router from "next/router";
 
 menuItems[0].isActive = true;
+
+const progress = new ProgressBar({
+  delay: 100,
+  color: "rgb(73, 115, 193)",
+});
+router.events.on("routeChangeStart", progress.start);
+router.events.on("routeChangeComplete", progress.finish);
+router.events.on("routeChangeError", progress.finish);
 
 export default function MyApp({ Component: Page, pageProps }) {
   return (
