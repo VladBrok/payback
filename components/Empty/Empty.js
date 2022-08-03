d2eecea8efed4eb31dca1414d33bda5cc421766d
Vlad Brok <vlad.brok99@gmail.com>
2022-08-03 18:13:51 +0300

    Add children and change styling of Empty component

--- components/Empty/Empty.js
+++ components/Empty/Empty.js
@@ -1,11 +1,14 @@
 import styles from "./Empty.module.scss";
 import Title from "components/Title";
 
-export default function Empty({ title, Icon, hint }) {
+export default function Empty({ title, Icon, hint, children }) {
   return (
     <div className={styles.container}>
-      <Title text={title} Icon={Icon} />
-      <p className={styles.hint}>{hint}</p>
+      <div className={styles.inner}>
+        <Title text={title} Icon={Icon} justifyContent="center" />
+        <p className={styles.hint}>{hint}</p>
+      </div>
+      {children}
     </div>
   );
 }
