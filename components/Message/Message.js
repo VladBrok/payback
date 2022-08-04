ee1e71ccfe7c557e6d5a2201c6c51aaf7c9f2611
Vlad Brok <vlad.brok99@gmail.com>
2022-08-04 17:00:49 +0300

    Adjust Message component

--- components/Message/Message.js
+++ components/Message/Message.js
@@ -1,16 +1,15 @@
 import styles from "./Message.module.scss";
 
-export default function Message({ message, userName }) {
+export default function Message({ message, username }) {
   const style = !message.from
     ? "info-message"
-    : message.from === userName
+    : message.from === username
     ? "message-from-me"
     : "message-from-friend";
 
   return (
     <li className={styles[style]}>
       <header>
-        <span className={styles.user}>{message.from}</span>
         <span className={styles.time}>{message.time}</span>
       </header>
       <p>{message.text}</p>
