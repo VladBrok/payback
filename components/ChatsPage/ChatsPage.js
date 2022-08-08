333c0846caa08a5fc4ac506d14d283e72f10423a
Vlad Brok <vlad.brok99@gmail.com>
2022-08-08 19:43:57 +0300

    Remove useEffect from callback

--- components/ChatsPage/ChatsPage.js
+++ components/ChatsPage/ChatsPage.js
@@ -1,6 +1,5 @@
 import styles from "./ChatsPage.module.scss";
 import User from "components/User";
-import Router from "components/Router";
 import Subpage from "components/Subpage";
 import Chat from "components/Chat";
 import NewMessages from "components/NewMessages";
@@ -14,12 +13,16 @@ import Head from "next/head";
 import { useSession } from "next-auth/react";
 import { FcApproval } from "react-icons/fc";
 import { useEffect, useState } from "react";
+import { useRouter } from "next/router";
 
 // todo: refactor (too large)
 function ChatsPage() {
   const [messages, setMessages] = useState([]);
   const [newMessageCount, setNewMessageCount] = useState(0);
   const [shouldScrollToBottom, setShouldScrollToBottom] = useState(true);
+  const router = useRouter();
+  const chatId = router.query?.id;
+  console.log(chatId);
   const {
     data: { user },
   } = useSession();
@@ -56,6 +59,16 @@ function ChatsPage() {
     }
   }, [messages]);
 
+  useEffect(() => {
+    if (!router.isReady) {
+      return;
+    }
+
+    if (chatId == null) {
+      setNewMessageCount(0);
+    }
+  }, [router.isReady, chatId]);
+
   function isScrolledToBottom() {
     return (
       Math.abs(doc().scrollHeight - doc().scrollTop - doc().offsetHeight) <= 3
@@ -82,37 +95,29 @@ function ChatsPage() {
     </li>
   ));
 
+  let content;
+  if (chatId == null) {
+    content = (
+      <>
+        <h1 className={styles.title}>Chats</h1>
+        <ul className={styles.list}>{chats}</ul>
+      </>
+    );
+  } else {
+    const chat = chatData.find(byId(chatId));
+    content = (
+      <Subpage title={chat.name}>
+        <Chat userId={userId} messages={messages} />
+      </Subpage>
+    );
+  }
+
   return (
     <>
       <Head>
         <title>Chats</title>
       </Head>
-
-      <Router>
-        {({ id: chatId }) => {
-          useEffect(() => {
-            if (chatId == null) {
-              setNewMessageCount(0);
-            }
-          }, [chatId]);
-
-          if (chatId == null) {
-            return (
-              <>
-                <h1 className={styles.title}>Chats</h1>
-                <ul className={styles.list}>{chats}</ul>
-              </>
-            );
-          }
-
-          const chat = chatData.find(byId(chatId));
-          return (
-            <Subpage title={chat.name}>
-              <Chat userId={userId} messages={messages} />
-            </Subpage>
-          );
-        }}
-      </Router>
+      {content}
     </>
   );
 }
