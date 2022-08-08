e2507725f0e399bb8724744aa3900bfbd9f835c4
Vlad Brok <vlad.brok99@gmail.com>
2022-08-08 21:02:19 +0300

    Load loading image eagerly

--- components/Loading/Loading.js
+++ components/Loading/Loading.js
@@ -3,6 +3,11 @@ import Image from "components/Image";
 
 export default function Loading() {
   return (
-    <Image src="/images/loading.svg" alt="Loading" className={styles.image} />
+    <Image
+      src="/images/loading.svg"
+      alt="Loading"
+      className={styles.image}
+      loading="eager"
+    />
   );
 }
