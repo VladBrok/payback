adca1ba207ddbe31c99c98b90a3ed741447805a5
Vlad Brok <vlad.brok99@gmail.com>
2022-08-10 13:58:29 +0300

    Remove div that wrapped Category

--- components/Category/Category.js
+++ components/Category/Category.js
@@ -11,23 +11,20 @@ export default function Category({
   imageSizeIncrease = "0px",
 }) {
   return (
-    <div
-      className={styles.container}
-      key={imageUrl}
-      style={{ "--img-size-increase": imageSizeIncrease }}
-    >
-      <Link href={`/categories/${encodeURIComponent(name)}`}>
-        <a className={styles.link} style={{ flexDirection }}>
-          <Image
-            className={styles.image}
-            // fixme: wrap in useMemo or useCallback ?
-            src={imageUrl ?? categories.find(byName(name)).imageUrl}
-            alt=""
-            objectFit="scale-down"
-          />
-          <span className={styles.name}>{name}</span>
-        </a>
-      </Link>
-    </div>
+    <Link href={`/categories/${encodeURIComponent(name)}`}>
+      <a
+        className={styles.container}
+        style={{ flexDirection, "--img-size-increase": imageSizeIncrease }}
+      >
+        <Image
+          className={styles.image}
+          // fixme: wrap in useMemo or useCallback ?
+          src={imageUrl ?? categories.find(byName(name)).imageUrl}
+          alt=""
+          objectFit="scale-down"
+        />
+        <span className={styles.name}>{name}</span>
+      </a>
+    </Link>
   );
 }
