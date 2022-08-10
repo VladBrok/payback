dcc230fe598c33096e34556a18f7625b05f1f06d
Vlad Brok <vlad.brok99@gmail.com>
2022-08-10 21:38:09 +0300

    Add non-breaking space to Error component

--- components/Error/Error.js
+++ components/Error/Error.js
@@ -1,5 +1,5 @@
 import styles from "./Error.module.scss";
 
 export default function Error({ children }) {
-  return <p className={styles.error}>{children}</p>;
+  return <p className={styles.error}>{children}&nbsp;</p>;
 }
