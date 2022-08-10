f88d60d342c36eb381d2a341c1f92abed3add42e
Vlad Brok <vlad.brok99@gmail.com>
2022-08-10 20:43:59 +0300

    Extract PriceInput component

---
+++ components/PriceInput/PriceInput.js
@@ -0,0 +1,15 @@
+import styles from "./PriceInput.module.scss";
+
+export default function PriceInput({ placeholder, value, onChange }) {
+  return (
+    <div className={styles.container}>
+      <input
+        className={styles.input}
+        type="number"
+        placeholder={placeholder}
+        onChange={onChange}
+        value={value}
+      />
+    </div>
+  );
+}
--- components/PriceRange/PriceRange.js
+++ components/PriceRange/PriceRange.js
@@ -1,29 +1,14 @@
 import styles from "./PriceRange.module.scss";
+import PriceInput from "components/PriceInput";
 
 export default function PriceRange({ onMinChange, onMaxChange, min, max }) {
   return (
-    <fieldset className={styles.container}>
+    <fieldset>
       <legend>Price</legend>
       <div className={styles["input-container"]}>
-        <div className={styles["input-wrapper"]}>
-          <input
-            className={styles.input}
-            type="number"
-            placeholder="From"
-            onChange={onMinChange}
-            value={min}
-          />
-        </div>
+        <PriceInput placeholder="From" onChange={onMinChange} value={min} />
         <span>—</span>
-        <div className={styles["input-wrapper"]}>
-          <input
-            className={styles.input}
-            type="number"
-            placeholder="To"
-            onChange={onMaxChange}
-            value={max}
-          />
-        </div>
+        <PriceInput placeholder="To" onChange={onMaxChange} value={max} />
       </div>
     </fieldset>
   );
