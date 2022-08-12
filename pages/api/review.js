6743f1d71c7e6cfd1a4b8d81b759e873f73307c7
Vlad Brok <vlad.brok99@gmail.com>
2022-08-12 10:12:07 +0300

    Add api endpoint for fetching reviews

---
+++ pages/api/review.js
@@ -0,0 +1,34 @@
+import prisma from "lib/prisma";
+
+// fixme: change error codes
+// fixme: protect with next-auth
+export default async function handler(req, res) {
+  let handle;
+
+  if (req.method === "GET") {
+    handle = handleGet;
+  } else if (req.method === "POST") {
+    handle = handlePost;
+  } else {
+    res.status(400).json({ error: `Method ${req.method} is not supported.` });
+    return;
+  }
+
+  try {
+    await handle(req, res);
+  } catch (er) {
+    console.log(er);
+    res.status(500).json({ error: "Fail" });
+  }
+}
+
+async function handleGet(req, res) {
+  const sellerId = +req.query.sellerId;
+  const reviews = await prisma.review.findMany({
+    where: { product: { userId: sellerId } },
+    include: { product: true, buyer: true },
+  });
+  res.status(200).json(reviews);
+}
+
+async function handlePost(req, res) {}
