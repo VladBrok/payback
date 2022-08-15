769b220734faddd2b72271c855af02d5cbf422fe
Vlad Brok <vlad.brok99@gmail.com>
2022-08-15 21:11:42 +0300

    Replace LinkToChat with button

--- pages/users/[id]/index.js
+++ pages/users/[id]/index.js
@@ -1,16 +1,18 @@
+import utilStyles from "styles/utils.module.scss";
 import Subpage from "components/Subpage";
 import User from "components/User";
 import ProductList from "components/ProductList";
 import Empty from "components/Empty";
 import Rating from "components/Rating";
-import LinkToChat from "components/LinkToChat";
 import Loading from "components/Loading";
 import ReviewLink from "components/ReviewLink";
 import { byUserId } from "lib/db/productFilters";
 import { FcInTransit } from "react-icons/fc";
+import { useSession } from "next-auth/react";
 import Head from "next/head";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
+import { makeChatId } from "lib/chat/chatId";
 
 export default function UserPage() {
   // fixme: dup with reviews.js
@@ -18,6 +20,7 @@ export default function UserPage() {
   const [user, setUser] = useState();
   const router = useRouter();
   const { id } = router.query;
+  const session = useSession();
 
   useEffect(() => {
     if (!router.isReady) {
@@ -26,6 +29,18 @@ export default function UserPage() {
 
     fetch(`/api/user?id=${id}`).then(async res => setUser(await res.json()));
   }, [router.isReady, id]);
+
+  function handleWriteMessageClick() {
+    // todo: dup (create AuthButton)
+    if (session.status !== "authenticated") {
+      router.push("/profile/signIn");
+      return;
+    }
+
+    const authenticatedUserId = session.data.user.id;
+    const chatId = makeChatId([authenticatedUserId, id]);
+    router.push(`/chats?id=${chatId}`);
+  }
 
   if (!user) {
     return <Loading />;
@@ -47,7 +62,12 @@ export default function UserPage() {
                 <ReviewLink userId={id}>{children}</ReviewLink>
               )}
             />
-            <LinkToChat userId={id} />
+            <button
+              className={utilStyles["button-tertiary"]}
+              onClick={handleWriteMessageClick}
+            >
+              Write a message
+            </button>
           </User>
         }
       >
