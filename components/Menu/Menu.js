59b69c668034d08cbaedd04d236894ec9bf575e3
Vlad Brok <vlad.brok99@gmail.com>
2022-08-17 18:24:52 +0300

    Specify menu-size variable

--- components/Menu/Menu.js
+++ components/Menu/Menu.js
@@ -2,19 +2,8 @@ import styles from "./Menu.module.scss";
 import itemsData from "data/menuItems";
 import MenuItem from "components/MenuItem";
 import { getRoot } from "lib/path";
-import { useEffect, useRef } from "react";
 
 export default function Menu({ activePath }) {
-  const containerRef = useRef();
-
-  useEffect(() => {
-    const menu = containerRef.current;
-    document.body.style.setProperty(
-      "--menu-size",
-      `${Math.min(menu.offsetWidth, menu.offsetHeight)}px`
-    );
-  }, []);
-
   activePath = getRoot(activePath);
 
   const items = itemsData.map(({ name, path, Icon }) => (
@@ -32,7 +21,7 @@ export default function Menu({ activePath }) {
 
   return (
     <nav>
-      <div className={styles.container} ref={containerRef}>
+      <div className={styles.container}>
         {items}
       </div>
     </nav>
