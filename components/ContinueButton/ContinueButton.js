3ca8c59f4edc530a41fb3bfcaaeb37fe46f8ca96
Vlad Brok <vlad.brok99@gmail.com>
2022-08-18 18:34:26 +0300

    Refactor SellPage component

---
+++ components/ContinueButton/ContinueButton.js
@@ -0,0 +1,5 @@
+import utilStyles from "styles/utils.module.scss";
+
+export default function ContinueButton() {
+  return <button className={utilStyles["button-primary"]}>Continue</button>;
+}
--- components/SellPage/SellPage.js
+++ components/SellPage/SellPage.js
@@ -1,5 +1,4 @@
 import styles from "./SellPage.module.scss";
-import utilStyles from "styles/utils.module.scss";
 import CategorySearch from "components/CategorySearch";
 import CategoryButton from "components/CategoryButton";
 import Subpage from "components/Subpage";
@@ -9,28 +8,116 @@ import PriceInput from "components/PriceInput";
 import ProductStatus from "components/ProductStatus";
 import PremiumIcon from "components/PremiumIcon";
 import Loading from "components/Loading";
+import ContinueButton from "components/ContinueButton";
 import { post } from "lib/api/client";
 import { formatMoney } from "lib/money";
 import { makePremiumPayment } from "lib/payment/client";
-import { useSession } from "next-auth/react";
 import { FcTemplate } from "react-icons/fc";
 import { useRouter } from "next/router";
 import Head from "next/head";
 import { useState, useEffect } from "react";
 
+const STEP_DATA = [
+  {
+    title: "Select category",
+    component: handle => (
+      <CategorySearch
+        searchBarLabel="Find"
+        category={props => <CategoryButton onClick={handle} {...props} />}
+      />
+    ),
+  },
+  {
+    title: "Upload photo",
+    component: handle => (
+      <FileForm onSubmit={handle} submitButton={<ContinueButton />} />
+    ),
+  },
+  {
+    title: "Specify title",
+    component: handle => (
+      <InputForm
+        key={1}
+        submitButton={<ContinueButton />}
+        max={70}
+        onSubmit={handle}
+        input={props => (
+          <input type="text" placeholder="Enter product title" {...props} />
+        )}
+      />
+    ),
+  },
+  {
+    title: "Specify description",
+    component: handle => (
+      <InputForm
+        key={2}
+        submitButton={<ContinueButton />}
+        max={300}
+        onSubmit={handle}
+        input={props => (
+          <textarea
+            placeholder="Enter product description"
+            rows="10"
+            cols="15"
+            {...props}
+          />
+        )}
+      />
+    ),
+  },
+  {
+    title: "Specify price",
+    component: (handle, props) => (
+      <InputForm
+        key={3}
+        submitButton={<ContinueButton />}
+        min={100}
+        max={100000}
+        initialValue="100"
+        onSubmit={handle}
+        hint={`Service charges are ${props.serviceChargesPercent}%`}
+        input={props => (
+          <PriceInput {...props} placeholder="Enter product price" />
+        )}
+      />
+    ),
+  },
+  {
+    title: "Select status",
+    component: (handle, props) => (
+      <>
+        <ProductStatus
+          key={4}
+          name="Premium"
+          description="Your product will be displayed on the main page"
+          Icon={PremiumIcon}
+          onClick={() => handle(true)}
+        >
+          Select for {formatMoney(props.premiumCost)}
+        </ProductStatus>
+        <ProductStatus
+          key={5}
+          name="Regular"
+          description="You can put the product up for sale for free, but the premium status will help you sell it faster"
+          Icon={FcTemplate}
+          onClick={() => handle(false)}
+        >
+          Select for free
+        </ProductStatus>
+      </>
+    ),
+  },
+  { title: "", component: () => <Loading /> },
+];
+
 const STEPS = {
-  CATEGORY: { value: 0, title: "Select category" },
-  PHOTO: { value: 1, title: "Upload photo" },
-  TITLE: { value: 2, title: "Specify title" },
-  DESCRIPTION: { value: 3, title: "Specify description" },
-  PRICE: { value: 4, title: "Specify price" },
-  PREMIUM: { value: 5, title: "Select status" },
-  END: { value: 6, title: "" },
+  START: 0,
+  END: STEP_DATA.length - 1,
 };
 
-// todo: refactor (useReducer ?)
-function SellPage({ serviceChargesPercent, premiumCost }) {
-  const [step, setStep] = useState(STEPS.CATEGORY);
+function SellPage(props) {
+  const [step, setStep] = useState(STEPS.START);
   const [category, setCategory] = useState();
   const [photoBlob, setPhotoBlob] = useState();
   const [title, setTitle] = useState();
@@ -38,13 +125,10 @@ function SellPage({ serviceChargesPercent, premiumCost }) {
   const [price, setPrice] = useState();
   const [isPremium, setIsPremium] = useState();
   const [paymentData, setPaymentData] = useState();
-  const {
-    data: { user },
-  } = useSession();
   const router = useRouter();
 
   useEffect(() => {
-    if (step.value !== STEPS.END.value) {
+    if (step !== STEPS.END) {
       return;
     }
 
@@ -66,34 +150,8 @@ function SellPage({ serviceChargesPercent, premiumCost }) {
     description,
     price,
     isPremium,
-    user.id,
     paymentData,
   ]);
-
-  function handleCategoryClick(value) {
-    setCategory(value);
-    setStep(STEPS.PHOTO);
-  }
-
-  function handlePhotoSubmit(value) {
-    setPhotoBlob(value);
-    goToNextStep();
-  }
-
-  function handleTitleSubmit(value) {
-    setTitle(value);
-    goToNextStep();
-  }
-
-  function handleDescriptionSubmit(value) {
-    setDescription(value);
-    goToNextStep();
-  }
-
-  function handlePriceSubmit(value) {
-    setPrice(value);
-    goToNextStep();
-  }
 
   function handlePremiumSelect(value) {
     const end = data => {
@@ -109,21 +167,33 @@ function SellPage({ serviceChargesPercent, premiumCost }) {
     }
   }
 
+  const handlers = [
+    withNextStep(setCategory),
+    withNextStep(setPhotoBlob),
+    withNextStep(setTitle),
+    withNextStep(setDescription),
+    withNextStep(setPrice),
+    handlePremiumSelect,
+  ];
+
+  function withNextStep(setter) {
+    return value => {
+      setter(value);
+      goToNextStep();
+    };
+  }
+
   function goToNextStep() {
-    setStep(cur => Object.values(STEPS).find(s => s.value === cur.value + 1));
+    setStep(cur => cur + 1);
   }
 
   function handleGoBack() {
-    if (step.value === 0) {
+    if (step === STEPS.START) {
       router.back();
     } else {
-      setStep(cur => Object.values(STEPS).find(s => s.value === cur.value - 1));
+      setStep(cur => cur - 1);
     }
   }
-
-  const continueButton = (
-    <button className={utilStyles["button-primary"]}>Continue</button>
-  );
 
   return (
     <>
@@ -131,86 +201,9 @@ function SellPage({ serviceChargesPercent, premiumCost }) {
         <title>Sell</title>
       </Head>
 
-      <Subpage title={step.title} onGoBack={handleGoBack}>
+      <Subpage title={STEP_DATA[step].title} onGoBack={handleGoBack}>
         <div className={styles.container}>
-          {step.value === STEPS.CATEGORY.value ? (
-            <CategorySearch
-              searchBarLabel="Find"
-              category={props => (
-                <CategoryButton onClick={handleCategoryClick} {...props} />
-              )}
-            />
-          ) : step.value === STEPS.PHOTO.value ? (
-            <FileForm
-              onSubmit={handlePhotoSubmit}
-              submitButton={continueButton}
-            />
-          ) : step.value === STEPS.TITLE.value ? (
-            <InputForm
-              key={STEPS.TITLE.value}
-              submitButton={continueButton}
-              max={70}
-              onSubmit={handleTitleSubmit}
-              input={props => (
-                <input
-                  type="text"
-                  placeholder="Enter product title"
-                  {...props}
-                />
-              )}
-            />
-          ) : step.value === STEPS.DESCRIPTION.value ? (
-            <InputForm
-              key={STEPS.DESCRIPTION.value}
-              submitButton={continueButton}
-              max={300}
-              onSubmit={handleDescriptionSubmit}
-              input={props => (
-                <textarea
-                  placeholder="Enter product description"
-                  rows="10"
-                  cols="15"
-                  {...props}
-                />
-              )}
-            />
-          ) : step.value === STEPS.PRICE.value ? (
-            <InputForm
-              key={STEPS.PRICE.value}
-              submitButton={continueButton}
-              min={100}
-              max={100000}
-              initialValue="100"
-              onSubmit={handlePriceSubmit}
-              hint={`Service charges are ${serviceChargesPercent}%`}
-              input={props => (
-                <PriceInput {...props} placeholder="Enter product price" />
-              )}
-            />
-          ) : step.value === STEPS.PREMIUM.value ? (
-            <>
-              <ProductStatus
-                key={`${STEPS.PREMIUM.value}-1`}
-                name="Premium"
-                description="Your product will be displayed on the main page"
-                Icon={PremiumIcon}
-                onClick={handlePremiumSelect.bind(null, true)}
-              >
-                Select for {formatMoney(premiumCost)}
-              </ProductStatus>
-              <ProductStatus
-                key={`${STEPS.PREMIUM.value}-2`}
-                name="Regular"
-                description="You can put the product up for sale for free, but the premium status will help you sell it faster"
-                Icon={FcTemplate}
-                onClick={handlePremiumSelect.bind(null, false)}
-              >
-                Select for free
-              </ProductStatus>
-            </>
-          ) : (
-            <Loading />
-          )}
+          {STEP_DATA[step].component(handlers[step], props)}
         </div>
       </Subpage>
     </>
