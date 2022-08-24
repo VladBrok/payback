38b3d9904adf6f228ee1d9e04edf35959b2066ca
Vlad Brok <vlad.brok99@gmail.com>
2022-08-24 16:52:03 +0300

    Extract getChat and getChats functions

--- pages/api/chat.js
+++ pages/api/chat.js
@@ -4,8 +4,7 @@ import { EVENTS, CHANNELS } from "lib/chat/constants";
 import prisma from "lib/db/prisma";
 import { transaction } from "lib/db/transaction";
 import { handle } from "lib/api/server";
-import { withPagination } from "lib/db/withPagination";
-import { CHAT_PAGE_SIZE } from "lib/sharedConstants";
+import { getChat, getChats } from "lib/db/chat";
 
 export default async function handler(req, res) {
   await handle(req, res, {
@@ -19,63 +18,16 @@ async function handleGet(req, res, session) {
   const id = req.query.id;
 
   if (id) {
-    const chat = await prisma.chat.findFirst({
-      where: { id },
-      include: {
-        users: {
-          where: { NOT: { userId } },
-          select: {
-            user: { select: { image: true, name: true, isVerified: true } },
-          },
-        },
-      },
-    });
+    const chat = await getChat(id, userId);
     if (!chat) {
       res.status(404).end();
     } else {
-      res.status(200).json(mapChat(chat));
+      res.status(200).json(chat);
     }
-    return;
+  } else {
+    const result = await getChats(req.query.pageCursor, userId);
+    res.status(200).json(result);
   }
-
-  const pageCursor = req.query.pageCursor;
-  const filter = { users: { some: { userId } } };
-
-  const result = await withPagination(
-    prisma.chat.findMany,
-    {
-      where: pageCursor
-        ? { AND: [filter, { id: { gt: pageCursor } }] }
-        : filter,
-      orderBy: { id: "asc" },
-      select: {
-        id: true,
-        messages: {
-          where: { AND: [{ wasRead: false }, { NOT: { userId } }] },
-        },
-        users: {
-          where: { NOT: { userId } },
-          select: {
-            user: { select: { image: true, name: true, isVerified: true } },
-          },
-        },
-      },
-    },
-    CHAT_PAGE_SIZE
-  );
-
-  result.pageData = result.pageData.map(mapChat);
-  res.status(200).json(result);
-}
-
-function mapChat(chat) {
-  return {
-    id: chat.id,
-    newMessageCount: chat.messages?.length,
-    isVerified: chat.users[0].user.isVerified,
-    image: chat.users[0].user.image,
-    name: chat.users[0].user.name,
-  };
 }
 
 async function handlePost(req, res) {
