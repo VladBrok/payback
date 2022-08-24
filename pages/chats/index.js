88032404ed94388a5ba4db0790f75bef4218fc73
Vlad Brok <vlad.brok99@gmail.com>
2022-08-24 17:04:57 +0300

    Add ssr to ChatsPage

--- pages/chats/index.js
+++ pages/chats/index.js
@@ -1,1 +1,15 @@
+import { getChats } from "lib/db/chat";
+import { getSessionUser, fetchServerSide } from "lib/serverSide";
+
 export { default } from "components/ChatsPage";
+
+export async function getServerSideProps(context) {
+  const userId = (await getSessionUser(context))?.id;
+  const chats = await fetchServerSide(() => getChats(null, userId));
+
+  return {
+    props: {
+      data: chats.data,
+    },
+  };
+}
