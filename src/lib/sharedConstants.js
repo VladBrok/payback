6b13b6a5a894aed03eb03e951fd706498f3c509c
Vlad Brok <vlad.brok99@gmail.com>
2022-08-25 20:33:03 +0300

    Increase page sizes

--- src/lib/sharedConstants.js
+++ src/lib/sharedConstants.js
@@ -1,9 +1,12 @@
 export const SUPPORT_ID = 1;
 export const MAX_RATING = 5;
+
 export const CURRENCY = "USD";
+
 export const BYTES_IN_MEGABYTE = 1000000;
 export const MAX_FILE_SIZE_IN_BYTES = BYTES_IN_MEGABYTE * 4;
-export const PRODUCT_PAGE_SIZE = 2;
-export const REVIEW_PAGE_SIZE = 1;
-export const MESSAGE_PAGE_SIZE = 5;
-export const CHAT_PAGE_SIZE = 2;
+
+export const PRODUCT_PAGE_SIZE = 15;
+export const REVIEW_PAGE_SIZE = 5;
+export const MESSAGE_PAGE_SIZE = 20;
+export const CHAT_PAGE_SIZE = 10;
