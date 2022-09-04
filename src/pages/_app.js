849cd467b481a7db6d649b7a3f51ac161bfabf72
Vlad Brok <vlad.brok99@gmail.com>
2022-09-04 19:13:42 +0300

    Add og tags

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -10,6 +10,7 @@ import router, { useRouter } from "next/router";
 import Head from "next/head";
 import { useEffect, useState } from "react";
 import { flushSync } from "react-dom";
+import { SITE_DESCRIPTION } from "lib/sharedConstants";
 
 export default function App({ Component: Page, pageProps }) {
   const [error, setError] = useState();
@@ -61,10 +62,18 @@ export default function App({ Component: Page, pageProps }) {
     <>
       <Head>
         <link rel="icon" type="image/png" href="/images/logo-small.png" />
+        <meta name="description" content={SITE_DESCRIPTION} />
         <meta
-          name="description"
-          content="Buy and sell products on Payback - a fully-featured e-commerce store."
+          property="og:title"
+          content="Payback - a fully-featured e-commerce store"
         />
+        <meta property="og:description" content={SITE_DESCRIPTION} />
+        <meta property="og:type" content="website" />
+        <meta
+          property="og:image"
+          content="https://payback-store.vercel.app/images/og-image.png"
+        />
+        <meta property="og:url" content="https://payback-store.vercel.app" />
       </Head>
 
       <SessionUserProvider value={pageProps.sessionUser}>
