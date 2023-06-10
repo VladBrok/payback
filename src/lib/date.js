e4b208441e748cb808c856d8b3098e5731f21a60
VladBrok <vlad.brok99@gmail.com>
2023-06-10 08:08:20 +0300

    implement custom "formatRelative" (because date-fns takes too much bundle)

--- src/lib/date.js
+++ src/lib/date.js
@@ -1,8 +1,28 @@
-import { formatRelative } from "date-fns";
 import { default as formatDistanceToNowFns } from "date-fns/formatDistanceToNow";
 
 export function formatRelativeToNow(dateStr) {
-  return formatRelative(new Date(dateStr), new Date());
+  const date = new Date(dateStr);
+  const time = Intl.DateTimeFormat(undefined, {
+    hour: "numeric",
+    minute: "numeric",
+  }).format(date);
+  const diff = differenceInCalendarDays(date, new Date());
+
+  let result = "";
+  if (diff === 0) {
+    result = `today at ${time}`;
+  } else if (diff === -1) {
+    result = `yesterday at ${time}`;
+  } else if (diff < -1 && diff >= -6) {
+    const weekday = date.toLocaleString("en-US", { weekday: "long" });
+    result = `last ${weekday} at ${time}`;
+  } else {
+    result = `${Intl.DateTimeFormat(undefined, {
+      dateStyle: "short",
+    }).format(date)} at ${time}`;
+  }
+
+  return result;
 }
 
 export function formatDistanceToNow(dateStr) {
@@ -11,4 +31,20 @@ export function formatDistanceToNow(dateStr) {
 
 export function getLastTwoDigitsOfNextYear() {
   return (new Date().getFullYear() + 1) % 100;
+}
+
+export function differenceInCalendarDays(later, earlier) {
+  const MS_PER_DAY = 1000 * 60 * 60 * 24;
+  const utcEarlier = Date.UTC(
+    earlier.getFullYear(),
+    earlier.getMonth(),
+    earlier.getDate()
+  );
+  const utcLater = Date.UTC(
+    later.getFullYear(),
+    later.getMonth(),
+    later.getDate()
+  );
+
+  return Math.floor((utcLater - utcEarlier) / MS_PER_DAY);
 }
