9d6321fdae5fc6580f39cf640dbfa789bce9f66c
VladBrok <vlad.brok99@gmail.com>
2023-06-10 08:58:35 +0300

    implement custom "formatDistanceToNow"

--- src/lib/date.js
+++ src/lib/date.js
@@ -1,5 +1,3 @@
-import { default as formatDistanceToNowFns } from "date-fns/formatDistanceToNow";
-
 export function formatRelativeToNow(dateStr) {
   const date = new Date(dateStr);
   const time = Intl.DateTimeFormat(undefined, {
@@ -26,7 +24,53 @@ export function formatRelativeToNow(dateStr) {
 }
 
 export function formatDistanceToNow(dateStr) {
-  return `${formatDistanceToNowFns(new Date(dateStr))} ago`;
+  const now = new Date();
+  const date = new Date(dateStr);
+  const nowUTC = Date.UTC(
+    now.getUTCFullYear(),
+    now.getUTCMonth(),
+    now.getUTCDate(),
+    now.getUTCHours(),
+    now.getUTCMinutes(),
+    now.getUTCSeconds()
+  );
+  const dateUTC = Date.UTC(
+    date.getUTCFullYear(),
+    date.getUTCMonth(),
+    date.getUTCDate(),
+    date.getUTCHours(),
+    date.getUTCMinutes(),
+    date.getUTCSeconds()
+  );
+  const diffMs = nowUTC - dateUTC;
+  const diffS = diffMs / 1000;
+
+  let result = "";
+  if (diffS < 30) {
+    result = "less than a minute";
+  } else if (diffS < 90) {
+    result = "1 minute";
+  } else if (diffS < 2670) {
+    result = `${Math.floor(diffS / 60)} minutes`;
+  } else if (diffS < 5370) {
+    result = "about 1 hour";
+  } else if (diffS < 86370) {
+    result = `about ${Math.floor(diffS / 60 / 60)} hours`;
+  } else if (diffS < 151170) {
+    result = "1 day";
+  } else if (diffS < 2591970) {
+    result = `${Math.floor(diffS / 60 / 60 / 24)} days`;
+  } else if (diffS < 3887970) {
+    result = "about 1 month";
+  } else if (diffS < 5183970) {
+    result = "about 2 months";
+  } else if (diffS < 31622370) {
+    result = `${Math.floor(diffS / 60 / 60 / 24 / 30)} months`;
+  } else {
+    result = "over 1 year";
+  }
+
+  return `${result} ago`;
 }
 
 export function getLastTwoDigitsOfNextYear() {
