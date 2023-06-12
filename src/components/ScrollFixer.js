db16955f0b7ae4f3f4c595f52e9f5cef3e75ad1d
VladBrok <vlad.brok99@gmail.com>
2023-06-12 12:57:42 +0300

    always scroll to top

--- src/components/ScrollFixer.js
+++ src/components/ScrollFixer.js
@@ -1,10 +1,8 @@
-import { useRouterScroll } from "@moxy/next-router-scroll";
 import { useRouter } from "next/router";
 import { useCallback, useEffect, useRef } from "react";
 
 export default function ScrollFixer({ children }) {
   const router = useRouter();
-  const { updateScroll } = useRouterScroll();
   const isBackRef = useRef(false);
 
   const shouldUpdateScroll = useCallback(() => {
@@ -24,17 +22,13 @@ export default function ScrollFixer({ children }) {
 
   useEffect(() => {
     const handleRouteChange = () => {
-      if (shouldUpdateScroll()) {
-        updateScroll();
-      } else {
-        window.scrollTo({ top: 0, left: 0, behavior: "instant" });
-      }
+      window.scrollTo({ top: 0, left: 0, behavior: "instant" });
     };
 
     router.events.on("routeChangeComplete", handleRouteChange);
 
     return () => router.events.off("routeChangeComplete", handleRouteChange);
-  }, [router.events, shouldUpdateScroll, updateScroll]);
+  }, [router.events, shouldUpdateScroll]);
 
   return <>{children}</>;
 }
