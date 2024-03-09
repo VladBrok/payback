75069b647151eeafcf24bfc523309bbc4eef6a83
VladBrok <vlad.brok99@gmail.com>
2024-03-10 00:37:36 +0100

    create local replicas

--- src/lib/db/prisma.js
+++ src/lib/db/prisma.js
@@ -6,12 +6,19 @@ let prisma;
 
 function makeClient() {
   const libsql = createClient({
-    url: `${process.env.TURSO_DATABASE_URL}`,
+    syncUrl: `${process.env.TURSO_DATABASE_URL}`,
+    url: "file:./prisma/dev.db",
     authToken: `${process.env.TURSO_AUTH_TOKEN}`,
   });
 
   const adapter = new PrismaLibSQL(libsql);
-  return new PrismaClient({ adapter });
+  const client = new PrismaClient({ adapter });
+  client.$use(async (params, next) => {
+    await libsql.sync();
+    const result = await next(params);
+    return result;
+  });
+  return client;
 }
 
 if (process.env.NODE_ENV === "production") {
