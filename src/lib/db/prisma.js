0885a0f4776555b861bbfd96ec3fcf6f948b360b
VladBrok <vlad.brok99@gmail.com>
2024-03-09 23:30:53 +0100

    migrate to sqlite with turso

--- src/lib/db/prisma.js
+++ src/lib/db/prisma.js
@@ -1,12 +1,24 @@
+import { createClient } from "@libsql/client";
+import { PrismaLibSQL } from "@prisma/adapter-libsql";
 import { PrismaClient } from "@prisma/client";
 
 let prisma;
 
+function makeClient() {
+  const libsql = createClient({
+    url: `${process.env.TURSO_DATABASE_URL}`,
+    authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+  });
+
+  const adapter = new PrismaLibSQL(libsql);
+  return new PrismaClient({ adapter });
+}
+
 if (process.env.NODE_ENV === "production") {
-  prisma = new PrismaClient();
+  prisma = makeClient();
 } else {
   if (!global.prisma) {
-    global.prisma = new PrismaClient();
+    global.prisma = makeClient();
   }
   prisma = global.prisma;
 }
