c5c3fbd6eea6bc01224599ee5c03c7dfc02b8bf2
VladBrok <vlad.brok99@gmail.com>
2024-03-10 11:43:07 +0100

    fix dummyjson images

--- next.config.js
+++ next.config.js
@@ -7,6 +7,7 @@ const nextConfig = {
       "fakestoreapi.com",
       "randomuser.me",
       "dummyjson.com",
+      "cdn.dummyjson.com",
       "avatars.yandex.net",
       "lh3.googleusercontent.com",
       "i.ibb.co",
